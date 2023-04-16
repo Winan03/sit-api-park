72d4612fe39994cb3f9d343c2021d68ef2455a40
Winan03 <JNACARINOA1@UPAO.EDU.PE>
2023-04-16 05:21:01 -0500

    estructura del proyecto api parking - inicial

---
+++ sit-parking/src/main/java/com/parking/apiparking/SitParkingApplication.java
@@ -0,0 +1,13 @@
+package com.parking.apiparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitParkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SitParkingApplication.class, args);
+    }
+
+}
---
+++ sit-parking/src/main/java/com/parking/apiparking/entity/Car.java
@@ -0,0 +1,4 @@
+package com.parking.apiparking.entity;
+
+public class Car {
+}
